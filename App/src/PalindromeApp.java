c4d60ab81ad0eda3417008238507fe40763fb4e3
manyasaxena23 <saxenamanya1210@gmail.com>
2026-02-27 16:22:27 +0530

    uc2 modified message

--- App/src/PalindromeApp.java
+++ App/src/PalindromeApp.java
@@ -4,18 +4,13 @@ public class PalindromeApp {
 
             public static void main(String[] args) {
 
-                // Hardcoded string (String Literal)
                 String word = "madam";
-
-                // Variable to store reversed string
                 String reversed = "";
 
-                // Reverse the string
                 for (int i = word.length() - 1; i >= 0; i--) {
                     reversed = reversed + word.charAt(i);
                 }
 
-                // Check if palindrome using if-else
                 if (word.equals(reversed)) {
                     System.out.println(word + " is a Palindrome.");
                 } else {
