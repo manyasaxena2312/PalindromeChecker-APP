9e43b98e8e2cb372cc1437c34864ab74f87a84a8
manyasaxena23 <saxenamanya1210@gmail.com>
2026-02-27 16:15:49 +0530

    uc2 modified message2

--- App/src/PalindromeApp.java
+++ App/src/PalindromeApp.java
@@ -1,8 +1,28 @@
 public class PalindromeApp {
     static void main() {
-        System.out.println("Welcome to the palindrome Checker Management    System");
-        System.out.println("version: 1.0");
-        System.out.println("System initialised successfully done");
+        public class UseCase2PalindromeCheckerApp {
+
+            public static void main(String[] args) {
+
+                // Hardcoded string (String Literal)
+                String word = "madam";
+
+                // Variable to store reversed string
+                String reversed = "";
+
+                // Reverse the string
+                for (int i = word.length() - 1; i >= 0; i--) {
+                    reversed = reversed + word.charAt(i);
+                }
+
+                // Check if palindrome using if-else
+                if (word.equals(reversed)) {
+                    System.out.println(word + " is a Palindrome.");
+                } else {
+                    System.out.println(word + " is NOT a Palindrome.");
+                }
+            }
+        }
 
     }
 }
