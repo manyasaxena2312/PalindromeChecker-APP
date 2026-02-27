ec7288509a7f72e817b26c929cd461f9a38cdbec
manyasaxena23 <saxenamanya1210@gmail.com>
2026-02-27 14:40:34 +0530

    welcome message

--- App/src/PalindromeApp.java
+++ App/src/PalindromeApp.java
@@ -1,6 +1,6 @@
 public class PalindromeApp {
     static void main() {
-        System.out.println("Welcome to the palindrome Checker Mangament System");
+        System.out.println("Welcome to the palindrome Checker Management    System");
         System.out.println("version: 1.0");
         System.out.println("System initialised successfully");
 
